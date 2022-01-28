4d09519e2fb2cd449293a8b4f506b65b3d6eac56
omarali18 <omarali1814000@gmail.com>
2022-01-28 09:25:31 +0600

    all service added

--- src/pages/Home/Experiences/Experiences.js
+++ src/pages/Home/Experiences/Experiences.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+import "./Experiences.css"
 
-const Experiences = () => {
+const Experiences = (props) => {
+    console.log("props=", props.experiences);
+    const {name,img,description,booking} = props.experiences
     return (
         <div>
-            <h1>this is experiences</h1>
+            <Col className='border ' md={12} sm={8}>
+                    <Row >
+                        <Col className=''md={4}><NavLink to="/addExperience"><img src={img} className='w-100 itemImg' alt="" /></NavLink></Col>
+                        <Col md={8} className='p-4'>
+                        <NavLink to="/addExperience" className="text-decoration-none "><h3 className='linkText'>{name}</h3></NavLink>
+                        <p className='text-justify'>{description}</p>
+                        <h5 style={{color: 'rgb(73, 73, 240)'}}>Booking date: {booking}</h5>
+                        </Col>
+                    </Row>
+            </Col>
         </div>
     );
 };
--- src/pages/Home/Experienceses/Experienceses.js
+++ src/pages/Home/Experienceses/Experienceses.js
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import Experiences from "../Experiences/Experiences"
 
 const Experienceses = () => {
-    const [experiences, setExperiences] = useState([])
+    const [experienceses, setExperienceses] = useState([])
 
     useEffect( ()=>{
         fetch("https://secure-fortress-87123.herokuapp.com/experiences")
         .then(res => res.json())
         .then(data=> {
-            setExperiences(data)
+            setExperienceses(data)
         })
     } ,[])
 
 
     return (
         <div style={{}}>
-            <Container className='border'>
-                <Row>
-                    <Col className='border ' md={12} sm={8}>sm=8</Col>
+            <Container className=''>
+                <Row className="g-5">
+                    {
+                        experienceses.map(experiences=><Experiences 
+                            key={experiences._id}
+                            experiences={experiences}
+                            />)
+                    }
+                    {/* <Col className='border ' md={12} sm={8}>sm=8</Col> */}
                 </Row>
             </Container>
         </div>
